9af50a3cb3345eff975178b370816c2ad34e1914
Oscar Rieken <oriekenjr@gmail.com>
2014-01-15 18:45:22 -0500

    adding d3 for visual

--- career_radar.js
+++ career_radar.js
@@ -31,6 +31,56 @@ if (Meteor.isClient) {
         return user;
     }
 });
+    // our d3 code goes here
+    Template.chart.rendered = function () {
+        var sampleSVG = d3.select("#chart_area")
+            .append("svg")
+            .attr("width", 600)
+            .attr("height", 600);
+
+        //Draw the line
+        sampleSVG.append("line")
+            .attr("x1", 50)
+            .attr("y1", 50)
+            .attr("x2", 50)
+            .attr("y2", 50)
+            .attr("stroke-width", 2)
+            .attr("stroke", "black");
+
+
+
+
+
+        sampleSVG.append("circle")
+            .style("stroke", "gray")
+            .style("fill", "white")
+            .attr("id", "d3_circle")
+            .attr("r", 5)
+            .attr("cx", 50)
+            .attr("cy", 50)
+            .on("mouseover", function(){d3.select(this).style("fill", "blue");})
+            .on("mouseout", function(){d3.select(this).style("fill", "white");})
+            .on("mousedown", grow);
+
+
+        function grow(){
+            d3.select(this)
+                .transition()
+                .delay(0)
+                .duration(500)
+                .attr("r", 40)
+                .each("end", animateSecondStep);
+        }
+
+        function animateSecondStep(){
+            d3.select(this)
+                .transition()
+                .duration(500)
+                .attr("r", 5);
+        }
+
+    }
+
 }
 
 
